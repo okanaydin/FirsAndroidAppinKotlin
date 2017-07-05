97827da100b9cd9bbfc43b369852d2b5c52916d5
OKAN <okanaydin1994@gmail.com>
2017-07-06 00:28:02 +0300

    Toast message added

--- app/src/main/java/com/example/okanaydin/firsandroidappinkotlin/MainActivity.kt
+++ app/src/main/java/com/example/okanaydin/firsandroidappinkotlin/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.okanaydin.firsandroidappinkotlin
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+    fun toastMe(view: View){
+
+        val myToast = Toast.makeText(this, "Hello Toast !", Toast.LENGTH_SHORT)
+        myToast.show()
+
+    }
+
 }
