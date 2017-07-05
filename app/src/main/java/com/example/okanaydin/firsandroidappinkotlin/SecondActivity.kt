911616b9eb56d4e63e1173861cbed24902ca9238
OKAN <okanaydin1994@gmail.com>
2017-07-06 01:06:19 +0300

    Second Activity designed

---
+++ app/src/main/java/com/example/okanaydin/firsandroidappinkotlin/SecondActivity.kt
@@ -0,0 +1,12 @@
+package com.example.okanaydin.firsandroidappinkotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
