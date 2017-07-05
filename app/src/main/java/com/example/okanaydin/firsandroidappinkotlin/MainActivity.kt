1910f362679b5cd1d5de68e0128d36b9b0c09aca
OKAN <okanaydin1994@gmail.com>
2017-07-06 01:02:37 +0300

    countMe added

--- app/src/main/java/com/example/okanaydin/firsandroidappinkotlin/MainActivity.kt
+++ app/src/main/java/com/example/okanaydin/firsandroidappinkotlin/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +18,18 @@ class MainActivity : AppCompatActivity() {
 
         val myToast = Toast.makeText(this, "Hello Toast !", Toast.LENGTH_SHORT)
         myToast.show()
+
+    }
+
+    fun countMe(view: View){
+
+        val countString = textView.text.toString()
+
+        var count: Int = Integer.parseInt(countString)
+        count++
+
+        textView.text = count.toString();
+
 
     }
 }
